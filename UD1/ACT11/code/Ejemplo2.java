0ffaeb3d946a736944d080f4f2b135657fc875e6
daniielynz <danielyanezdm2@gmail.com>
2023-11-06 10:51:18 +0100

    Ejercicio 11 corregido

--- UD1/ACT11/code/Ejemplo2.java
+++ UD1/ACT11/code/Ejemplo2.java
@@ -1,31 +1,36 @@
+package application;
 
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Ejemplo2 {
-	public static void main(String[] args){
-		Runtime r=Runtime.getRuntime();
-		String comando="ls -al";
-	    	Process p=null;
-		try {
-			p = r.exec (comando);
-			InputStream is = p.getInputStream();
-			BufferedReader br = new BufferedReader (new InputStreamReader(is));
-			String linea;
-			while((linea = br.readLine()) != null)
-				System.out.println(linea);
-			br.close();
-			}
-		catch (Exception e) {
-				e.printStackTrace();
-		}
-		///// Comprobación: Valor= 0 bien, Valor = - 1 mal
-		int exitVal;
-		try {
-				exitVal=p.waitFor();
-				System.out.println ("Valor de Salida "+exitVal);
-		}  catch (InterruptedException e){
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        // Comando para listar archivos en el directorio actual (Linux/Mac)
+        String comando = "ls -al";
+        
+        ProcessBuilder processBuilder = new ProcessBuilder(comando.split(" "));
+        processBuilder.redirectErrorStream(true);
+        
+        try {
+            Process proceso = processBuilder.start();
+            
+            // Lee la salida del proceso
+            InputStream is = proceso.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+            
+            // Espera a que el proceso termine y obtiene el código de salida
+            int exitCode = proceso.waitFor();
+            System.out.println("Valor de Salida: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
