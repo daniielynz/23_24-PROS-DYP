f10e09ebf534f2216a2206f03af34b94492dba41
daniielynz <danielyanezdm2@gmail.com>
2023-11-06 10:20:47 +0100

    Corregido ejercicio 10

--- UD1/ACT10/code/Ejercicio10.c
+++ UD1/ACT10/code/Ejercicio10.c
@@ -4,40 +4,48 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-// Metodo que devuelve los mensajes que saldran por consola
-void signal_handler(int signo) {
+int count = 0;
+
+// Función para manejar la señal SIGUSR1 en el padre
+void sigusr1_handler(int signo) {
     if (signo == SIGUSR1) {
-        printf("Padre recibe señal...10\n");
-    } else if (signo == SIGKILL) {
-        printf("Terminado (killed)\n");
-        exit(0);
+        printf("Padre recibe señal...%d\n", signo);
     }
 }
 
 int main() {
-    // Configurar el manejador de señales para SIGUSR1
-    signal(SIGUSR1, signal_handler);
-    
-    pid_t child_pid;
-    child_pid = fork();
+    // Registra el manejador de señal para SIGUSR1 en el padre
+    signal(SIGUSR1, sigusr1_handler);
 
-    if (child_pid < 0) {
-        // si el pid es negativo, da error
+    pid_t child_pid;
+
+    // Crea un proceso hijo
+    if ((child_pid = fork()) < 0) {
         perror("Error al crear el proceso hijo");
         exit(1);
-    } else if (child_pid == 0) {
-        // Esperar para que el proceso padre esté listo para recibir señales
-        sleep(2);
-        // Enviar 3 señales SIGUSR1 al proceso padre
+    }
+
+    if (child_pid == 0) {
+        // Este es el proceso hijo
+        sleep(1);  // Espera un segundo para asegurarse de que el padre esté listo
+
+        // Envía tres señales SIGUSR1 al proceso padre
         for (int i = 0; i < 3; i++) {
             kill(getppid(), SIGUSR1);
-            // un segundo de espera
-            sleep(1); 
+            sleep(1);
         }
 
-        // Utilizamos SIGKILL para matar el proceso padre
+        // Envía una señal SIGKILL para terminar el proceso padre
         kill(getppid(), SIGKILL);
+    } else {
+        // Este es el proceso padre
+        while (1) {
+            // Espera a recibir señales
+            pause();
+        }
     }
 
     return 0;
 }
+
+
